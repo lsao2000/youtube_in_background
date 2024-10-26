bb50fb778fff815bca07cf111d21212945e5374f
lsao2000 <lahcenenligne@gmail.com>
2024-10-26 11:41:05 +0100

    release the first version

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,22 +9,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:youtube_inbackground_newversion/main.dart';
+import 'package:youtube_inbackground_newversion/view/HomePage.dart';
+import 'package:youtube_inbackground_newversion/view/MainPage.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Y&B testing', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
+    //expect(find.text('1'), findsNothing);
+    expect(find.byType(MainPage), findsOneWidget);
+    //final button = find.byIcon(Icons.home);
+    //tester.tap(button)
+    //expect(find.byType(HomePage), findsOneWidget);
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text("Search Something"), findsOneWidget);
+    //// Verify that our counter has incremented.
+    //expect(find.text('1'), findsOneWidget);
   });
 }
